ffe861f40eee66458dc9de5315819666a9e86f40
Gavin Cheng <gavincheng01@gmail.com>
2017-10-24 16:45:58 -0400

    Added car table with php

--- index.php
+++ index.php
@@ -9,18 +9,77 @@
 	<title> PHP STUFF </title>
 </head>
 <body class="container">
-	<h1> PHP used car website </h1>
-	<h2> Quality Pre-owed vehicles ... powered by PHP!</h2>
-
 
 <?php
 
+	// Connect to database -get database handle.
+	function getDb() {
+		$db = pg_connect("host=localhost port=5432 dbname=cars_dev user=carsuser password=carscarscars");
+		return $db;
+	}
 
-echo "hello world!\n";
+	// Make a request.
+	function getInventory() {
+		$request = pg_query(getDb(), "
+			SELECT i.id, i.year, i.mileage, mo.name AS model, mo.doors, ma.name AS make, c.name AS color
+			FROM inventory i
+			JOIN models mo ON i.model_id = mo.id
+			JOIN makes ma ON mo.make_id = ma.id
+			JOIN color c ON i.color_id = c.id;
+		");
+		// Return a fetch to use the data.
+		return pg_fetch_all($request);
+	}
 
 
 ?>
 
 
+	<h1> PHP used car website </h1>
+	<h2> Quality Pre-owed vehicles ... powered by PHP!</h2>
+
+	<table class="table">
+		<tr>
+			<th>ID</th>
+			<th>YEAR</th>
+			<th>Make</th>
+			<th>Model</th>
+			<th>Color</th>
+			<th>Doors</th>
+			<th>Mileage</th>
+		</tr>
+<?php
+				// $var is an associative arry
+	foreach (getInventory() as $car) {
+
+		echo "<tr>";
+		echo "<td>".$car['id']."</td>";
+		echo "<td>".$car['year']."</td>";
+		echo "<td>".$car['make']."</td>";
+		echo "<td>".$car['model']."</td>";
+		echo "<td>".$car['color']."</td>";
+		echo "<td>".$car['doors']."</td>";
+		echo "<td>".$car['mileage']."</td>";
+		echo "</tr>\n";
+	
+	
+
+	}
+
+
+?>
+
+
+	</table>
+
+
+
+
+
+
+
+
+
 
 </body>
+</html>
